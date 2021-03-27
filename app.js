b37bf25cde1c7c5ca7916c55ff3c49c73b31b7a2
tradecoder <thetradecoder@gmail.com>
2021-03-27 13:16:11 +0600

    included 0 to passwords

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const value = document.getElementById('value');
 value.textContent='YoUrPaSswoRd';
 const generator = document.getElementById('generator');
-const numbers = "123456789".split('');
+const numbers = "1234567890".split('');
 const alphab = [...Array(26)].map((_, y) => String.fromCharCode(y+65)); 
 const alphas = [...Array(26)].map((_, y) => String.fromCharCode(y+97));
 const sym = ['~', '!', '#','*'];
